88da568bab434544553a59c46334a002860fb1c6
shaybay <pacificconnect310@gmail.com>
2023-01-30 22:51:35 -0800

    started react app and reconfigured file structure to begin devloping header componant

---
+++ tinder-clone/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './App.css';
+import Header from "./Header";
+import "./Header.css"
+
+function App() {
+  return (
+    // BEM class naming convention calls us to change App to app (lower case 'a')
+    <div className="app">
+      
+      <Header/>
+
+      {/* tinder profile cards */}
+
+      {/* swiping options bar */}
+
+    </div>
+  );
+}
+
+export default App;
